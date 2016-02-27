f90405214e5ec6cb46ffb8e208e88422b72e7bca
rod7val <rod7val@gmail.com>
2016-02-27 14:43:07 -0300

    added story controller

--- app/routes/api.js
+++ app/routes/api.js
@@ -5,6 +5,7 @@ var secretKey = config.secretKey;
 
 // Controllers
 var userController = require('../controllers/user_controller');
+var storyController = require('../controllers/story_controller');
 
 module.exports = function (app, express) {
 
@@ -29,70 +30,30 @@ module.exports = function (app, express) {
     /**
      * Get all user stories from server..
      */
-    api.get('/stories', function (req, res) {
-        Story.find({'publishStatus': 1}, function (err, stories) {
-            if (err) {
-                res.send(err);
-                return
-            }
-            res.json(stories);
-        });
-    });
+    api.get('/stories', storyController.getAll);
 
     /**
      * Get specific story details from server
      * with provided story id.
      */
-    api.get('/story', function (req, res) {
-        Story.find({_id: req.param('id')}, function (err, story) {
-            if (err) {
-                res.send(err);
-                return
-            }
-            res.json(story);
-        });
-    });
+    api.get('/story', storyController.story);
 
     /**
      * Removing the selected story
      * from server.
      */
-    api.get('/remove_story', function (req, res) {
-        Story.remove({_id: req.param('id')}, function (err) {
-            if (err) {
-                res.send(err);
-                return
-            }
-            res.json({message: "Successfuly removed", type: "success"});
-        });
-    });
+    api.get('/remove_story', storyController.remove_story);
 
     /**
      * Search all the stories and return data that matches with
      * provided query and criteria.
      **/
-    api.get('/search_story', function (req, res) {
-        Story.find({$and: [{'content': new RegExp(req.param('query'), 'i')}, {'publishStatus': 1}]}, function (err, stories) {
-            if (err) {
-                res.send(err);
-                return
-            }
-            res.json(stories);
-        });
-    });
+    api.get('/search_story', storyController.search_story);
 
     /**
      * Search stories according to the given category.
      */
-    api.get('/search_story_by_category', function (req, res) {
-        Story.find({$and: [{category: req.param('category')}, {'publishStatus': 1}]}, function (err, stories) {
-            if (err) {
-                res.send(err);
-                return
-            }
-            res.json(stories);
-        });
-    });
+    api.get('/search_story_by_category', storyController.search_story_by_category);
 
     /**
      * Search with the username and check if there is a user available or not.
@@ -125,35 +86,12 @@ module.exports = function (app, express) {
     /**
      * Create new story.
      */
-    api.post('/story', function (req, res) {
-        var story = new Story({
-            owner: req.decoded._id,
-            title: req.body.title,
-            content: req.body.content,
-            category: req.body.category,
-            publishStatus: req.body.publishStatus
-        });
-        story.save(function (err) {
-            if (err) {
-                res.status(500).send(err);
-                return
-            }
-            res.json({message: "New Story Created", type: "success", code: 200});
-        });
-    });
+    api.post('/story', storyController.new);
 
     /**
      * Get all user stories from server with registered user id..
      */
-    api.get('/story_of_user', function (req, res) {
-        Story.find({owner: req.decoded._id}, function (err, stories) {
-            if (err) {
-                res.send(err);
-                return
-            }
-            res.json(stories);
-        });
-    });
+    api.get('/story_of_user', storyController.story_of_user);
 
     /**
      * Getting about logged user.
@@ -163,23 +101,7 @@ module.exports = function (app, express) {
     /**
      * Update story model with data from req.
      **/
-    api.post('/update_story', function (req, res) {
-        var story = {
-            owner: req.decoded._id,
-            title: req.body.title,
-            content: req.body.content,
-            category: req.body.category,
-            publishStatus: req.body.publishStatus
-        };
-        console.log("Story : ", story);
-        Story.findOneAndUpdate({'_id': req.body.storyId}, story, function (err) {
-            if (err) {
-                res.status(500).send(err);
-                return
-            }
-            res.json({message: "Story updated successfully"});
-        });
-    });
+    api.post('/update_story', storyController.update_story);
 
     /**
      * Returning the API.
