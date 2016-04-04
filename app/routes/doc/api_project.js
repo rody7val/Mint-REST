f5ec05a8ff7060d5283d02079352508b9e00f867
rod7val <rod7val@gmail.com>
2016-04-04 18:35:00 -0300

    added route /documentation

---
+++ app/routes/doc/api_project.js
@@ -0,0 +1,13 @@
+define({
+  "name": "Mint-REST",
+  "version": "1.0.0",
+  "description": "Una API RESTful usando MongoDB, Node.js y express",
+  "apidoc": "0.2.0",
+  "sampleUrl": false,
+  "generator": {
+    "name": "apidoc",
+    "time": "2016-04-04T18:05:49.817Z",
+    "url": "http://apidocjs.com",
+    "version": "0.15.1"
+  }
+});
